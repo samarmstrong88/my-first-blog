808c426c6fd48b9ccf21e046f487f6b4921b9a92
= <=>
2017-08-23 02:18:58 -0700

    removed .pyc files

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.utils import timezone
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-    return render (request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render (request, 'blog/post_list.html', {'posts':posts})
